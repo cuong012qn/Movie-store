4417f5d67e25ecca55850f32159593bc0e953dd1
cuong012qn <cuong012qn121@gmail.com>
2021-05-03 23:33:19 +0700

    Rename

---
+++ Movie-Store-API/Services/Interface/IUserService.cs
@@ -0,0 +1,25 @@
+﻿using Movie_Store_Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Store_API.Services.Interface
+{
+    public interface IUserService
+    {
+        Task<string> AuthencationUserAsync(string username, string password, string token);
+
+        Task<User> GetUserByIDAsync(string id);
+
+        Task<User> Login(string username, string password);
+
+        Task AddUserAsync(User user);
+
+        void RemoveUser(User user);
+
+        void UpdateUser(User user);
+
+        Task SaveChangesAsync();
+    }
+}
